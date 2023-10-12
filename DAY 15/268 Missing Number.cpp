699883e35922039a90c30ade8cd4138c66eb97ff
Shubham Goyal <122973606+shubham-goyal-0811@users.noreply.github.com>
2023-10-13 00:45:27 +0530

    Leetcode 268 Missing Number.cpp

--- DAY 15/268 Missing Number.cpp
+++ DAY 15/268 Missing Number.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        // cout<<nums.size();
         for(int i=0;i<nums.size();i++){
             if(i!=nums[i])
                 return i;
