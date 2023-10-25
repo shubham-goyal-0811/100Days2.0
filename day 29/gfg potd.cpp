9b1db8228a39c5c477189622b49fe0552398744b
Shubham Goyal <122973606+shubham-goyal-0811@users.noreply.github.com>
2023-10-25 23:52:47 +0530

    gfg potd

---
+++ day 29/gfg potd.cpp
@@ -0,0 +1,18 @@
+int n = str.size();
+        vector<vector<bool>> dp(n, vector<bool> (n, true));
+        for(int k = 1; k < n; k++){
+            for(int i = 0; i < n - k; i++){
+                int j = i + k;
+                dp[i][j] = (str[i] == str[j] and dp[i + 1][j - 1]);
+            }
+        }
+        vector<int> cuts(n + 1, n - 1);
+        cuts[0] = -1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                if(dp[j - 1][i - 1]){
+                    cuts[i] = min(cuts[i], cuts[j - 1] + 1);
+                }
+            }
+        }
+        return cuts[n];
