ee89ccc7cd132a3bfb13243f2637c33429d4cca6
Shubham Goyal <122973606+shubham-goyal-0811@users.noreply.github.com>
2023-10-29 22:36:16 +0530

    GFG potd

---
+++ day 33/gfg potd.cpp
@@ -0,0 +1,9 @@
+    bool checkKthBit(int n, int k)
+    {
+        // Your code here
+        // It can be a one liner logic!! Think of it!!
+        if((n&(1<<(k))) != 0){
+            return true;
+        }
+        return false;
+    }
