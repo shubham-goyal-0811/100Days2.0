554cdb099c57410e029d08900410d817088ac3b0
Shubham Goyal <122973606+shubham-goyal-0811@users.noreply.github.com>
2024-01-01 15:46:08 +0530

    Leetcode 455 Assign Cookies

---
+++ day 96/Leetcode 455 assign cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int i =0;
+        int j =0;
+        int ans = 0;
+        sort (g.begin() , g.end());
+        sort (s.begin() , s.end());
+
+        while (i < g.size() and j < s.size()){
+            if (s[j]>=g[i]) {
+                ans++;
+                i++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
+        return ans;
+    }
+};
